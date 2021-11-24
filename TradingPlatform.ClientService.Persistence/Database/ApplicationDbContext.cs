4beeeb3c5adf68ad1e2e15d8902d76b46924e6f5
RomanYatskovyna <roman.yatskovyna@gmail.com>
2021-11-24 19:31:13 +0200

    Modified context to work with applicationUser

--- TradingPlatform.ClientService.Persistence/Database/ApplicationDbContext.cs
+++ TradingPlatform.ClientService.Persistence/Database/ApplicationDbContext.cs
@@ -3,10 +3,11 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using TradingPlatform.ClientService.Domain.Entities;
 
 namespace TradingPlatform.ClientService.Persistence.Database
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
